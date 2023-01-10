fcdb0c84e6b79d852ff9cb720145fb22cfa08448
Nickunj Chopra <nickunjchopra@gmail.com>
2023-01-10 15:11:46 +0530

    Working on having Playwright emulate a mobile device for better screenshots.

--- screenshot.py
+++ screenshot.py
@@ -8,6 +8,8 @@ def screenshot(link, objID, objType):
   with sync_playwright() as p:
     browser = p.chromium.launch()
     page = browser.new_page()
+    device = p.devices['iPhone 12']
+    page = browser.new_page(**device, )
     link = link if 'https://www.reddit.com/' in link else 'https://www.reddit.com{}'.format(link)
     page.goto(link)
 
@@ -24,3 +26,5 @@ def screenshot(link, objID, objType):
     browser.close()
 
   return filename
+
+screenshot('https://www.reddit.com/r/AskReddit/comments/107xayi/you_job_is_to_pick_the_most_awful_actor_to_play/', '107xayi', 'post')
