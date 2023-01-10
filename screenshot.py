50c77f2f4f286112c2af2dc25099cf7b8886b424
Nickunj Chopra <nickunjchopra@gmail.com>
2023-01-10 16:01:17 +0530

    Perfecto!

--- screenshot.py
+++ screenshot.py
@@ -9,22 +9,24 @@ def screenshot(link, objID, objType):
     browser = p.chromium.launch()
     page = browser.new_page()
     device = p.devices['iPhone 12']
-    page = browser.new_page(**device, )
+    page = browser.new_page(**device, color_scheme='dark')
     link = link if 'https://www.reddit.com/' in link else 'https://www.reddit.com{}'.format(link)
     page.goto(link)
+    page.wait_for_load_state('networkidle')
 
-    accountLocator = 'i.icon-caret_down'
-    darkModeLocator = 'button i.icon-night'
-
-    locatorTag = '[data-testid="post-container"]' if objType == 'post' else '.Comment'
+    locatorTag = 'shreddit-post' if objType == 'post' else 'shreddit-comment-tree'
+    continueButtonTag = 'button#secondary-button.continue'
 
     if page.locator(locatorTag).first.is_visible():
-      page.locator(accountLocator).first.click()
-      page.locator(darkModeLocator).first.click()
+      page.locator(continueButtonTag).click()
+      if objType != 'post':
+        page.evaluate('''(commentChildren) => document.querySelector(commentChildren).remove()''', '[slot="children"]')
+        page.evaluate('''(commentTools) => document.querySelector(commentTools).remove()''', 'shreddit-comments-page-tools')
       page.locator(locatorTag).first.screenshot(path=filename)
     
     browser.close()
 
   return filename
 
+screenshot('https://www.reddit.com/r/AskReddit/comments/107xayi/comment/j3p0vng/', 'j3p0vng', 'comment')
 screenshot('https://www.reddit.com/r/AskReddit/comments/107xayi/you_job_is_to_pick_the_most_awful_actor_to_play/', '107xayi', 'post')
